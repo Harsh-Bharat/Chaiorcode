dc57a2a1b39eed21218fbf4ac066f6a55a9c4463
HARSH HARSH <harshji511@gmail.com>
2024-12-13 18:20:17 +0530

    Added new feature for user authentication

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 4000
+
+const port = 4001
 
 app.get('/', (req, res) => {
   res.send('Hello Worlsjhjd!');
@@ -9,6 +11,6 @@ app.get('/', (req, res) => {
 app.get('/harsh',(req,res)=>{
   res.send('Hello Worldszkj!');
 })
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
